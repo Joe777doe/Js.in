72a9ba51f4887548671e40c61f5c50b9863df67b
Joe777doe <darkk497@gmail.com>
2025-01-26 14:09:38 +0000

    work

---
+++ 1/variable.js
@@ -0,0 +1,7 @@
+const accountID = 14453
+let accountEmail = "hitesh@gmail,com"
+var accountPassword = "2522"
+accountcity = "Jaipur"
+
+console.log(accountID)
+console.table([accountID, accountEmail, accountPassword, accountcity])
